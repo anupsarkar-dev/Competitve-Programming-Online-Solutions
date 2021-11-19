d7540511ea51bda24336032cc664b25fa23bcf80
Anup Sarkar <14172259+MeAnupSarkar@users.noreply.github.com>
2021-11-19 20:57:50 +0600

    Problem 2 Solved

--- LeetCode/P2_AddTwoNumber/Solutions/C#/AddTwoNumber/ListNode.cs
+++ LeetCode/P2_AddTwoNumber/Solutions/C#/AddTwoNumber/ListNode.cs
@@ -3,11 +3,12 @@ namespace LinkedList
     public class ListNode
     {
         public int val;
-        public ListNode next;
-        public ListNode(int val = 0, ListNode next = null)
+        public ListNode? next;
+        public ListNode(int val = 0, ListNode? next = null)
         {
             this.val = val;
             this.next = next;
         }
+
     }
 }
--- LeetCode/P2_AddTwoNumber/Solutions/C#/AddTwoNumber/Program.cs
+++ LeetCode/P2_AddTwoNumber/Solutions/C#/AddTwoNumber/Program.cs
@@ -2,33 +2,83 @@
 using LinkedList;
 
 
-var node0 = new ListNode(2,null);
-var node1 = new ListNode(4,node0);
-var current = new ListNode(3,node1);
+var output = AddTwoNumbers(GETINPUT1(), GETINPUT2());
 
+//RESULT
+while (output != null)
+{
+    System.Console.WriteLine(output.val);
+    output = output.next;
+}
 
-var node2_0 = new ListNode(5,null);
-var node2_1 = new ListNode(6,node0);
-var current2 = new ListNode(4,node1);
-  
-var output = AddTwoNumbers(current, current2);
+ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+{ 
+    ListNode? resultHead  = null;
+    ListNode? currentElem = null ;
 
- while(current != null)
- {
-    System.Console.WriteLine( current.val );
-    current= current.next;
- }
- 
+    int i = 0; var carry = 0; var temp = 0;
 
-
-
-ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
-
-    while(current != null)
+    while (l1 != null || l2 != null)
     {
-        System.Console.WriteLine( current.val );
-        current= current.next;
-    }
+        temp = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry ;
+         
+        carry = temp > 9 ? 1: 0;
 
+        if (i == 0)
+        {
+            resultHead = new ListNode(temp > 9 ? temp - 10 : temp);
+            currentElem = resultHead;
+        }
+        else
+        {
+            currentElem.next = new ListNode(temp > 9 ? temp - 10 : temp);
+            currentElem = currentElem.next;
+        }
+
+        if( l1 is not null) l1.val = 0;
+        if( l2 is not null) l2.val = 0;
         
+        l1 =  l1?.next;
+        l2 =  l2?.next;
+
+        if(l1 is null && l2 is null && carry > 0)
+        {
+            currentElem.next = new ListNode(carry);
+            currentElem = currentElem.next;
+        } 
+
+        i++;
+    }
+    
+    return resultHead;
+}
+
+
+
+ListNode GETINPUT1()
+{
+    ListNode head = new ListNode(2);
+    ListNode current = head;
+
+    current.next = new ListNode(4);
+    current = current.next;
+
+    current.next = new ListNode(3);
+    current = current.next;
+
+    return head;
+}
+
+ListNode GETINPUT2()
+{
+    ListNode head2 = new ListNode(5);
+    ListNode current2 = head2;
+
+    current2.next = new ListNode(6);
+    current2 = current2.next;
+
+    current2.next = new ListNode(4);
+    current2 = current2.next;
+
+    return head2;
 }
