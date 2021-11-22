4a4da2e290bccb6e3c9cdd6462656593d19d0185
mshsusmoy <mshsusmoy@gmail.com>
2021-11-22 16:44:41 +0600

    Problem 278. First Bad Submission Solved

---
+++ LeetCode/Python/Problem#278. First Bad Version/First Bad Version.py
@@ -0,0 +1,18 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        low = 1
+        high = n
+        while(low < high):
+            mid = (low + high)/2
+            if isBadVersion(mid):
+                high = mid
+            else:
+                low = mid + 1
+        return int(high)
+        
+        
