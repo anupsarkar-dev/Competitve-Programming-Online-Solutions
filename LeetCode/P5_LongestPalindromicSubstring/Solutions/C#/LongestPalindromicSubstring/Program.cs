aa9935ac39de6138023088d84ffad69bca6c80ed
Anup Sarkar <14172259+MeAnupSarkar@users.noreply.github.com>
2021-12-25 22:32:05 +0600

    Problem 5 Solved

--- LeetCode/P5_LongestPalindromicSubstring/Solutions/C#/LongestPalindromicSubstring/Program.cs
+++ LeetCode/P5_LongestPalindromicSubstring/Solutions/C#/LongestPalindromicSubstring/Program.cs
@@ -1,5 +1,61 @@
-﻿
+﻿public class Solution {
+    
+ public  String LongestPalindrome(String s) {
+	 
+     
+    var max = 0;
+    var len = s.Length;
 
-var str = "abc";
+    if (len == 1) return s;
+     
+    
+    if (s == new String(s.Reverse().ToArray())) return s;
 
-System.Console.WriteLine( str.ToString().Reverse());
+    var start = 0;
+    var end = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        //subString = $"{s[i]}";
+        var slen = 1;
+        for (int j = i + 1; j < len; j++)
+        {
+            // subString = s[i..(j + 1)];
+            slen++;
+
+            int k = 0;
+
+            var isPalinDrome = true;
+
+            while (k < slen / 2)
+            {
+                if (s[i + k] != s[(slen - (k + 1)) + i])
+                {
+                    isPalinDrome = false;
+                    break;
+                }
+
+                k++;
+            }
+
+            if (isPalinDrome)
+            {
+                if (slen > max)
+                {
+                    max = slen;
+                    // result = subString;
+                    // result = s[i..(j + 1)];
+                    start = i;
+                    end = (j + 1);
+                }
+
+            }
+        }
+
+        slen = 0;
+
+    }
+    return s[start..end] == "" ? $"{s[0]}" : s[start..end];
+ 
+ }
+}
